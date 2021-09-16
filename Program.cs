205144890bc476f0cc1971fb103b2c1466483e49
kavitaparsewar <parsewarkavita@gmail.com>
2021-09-17 00:10:38 +0530

    UC2 added

---
+++ Program.cs
@@ -0,0 +1,13 @@
+﻿using System;
+
+namespace Day4_SnakeLadder
+{
+    class Program
+    {
+        public static void Main(string[] args)
+        {
+            //UC1_SinglePlayer.singleplayer();
+            UC2_Between1to6.randomplayer();
+        }
+    }
+}
