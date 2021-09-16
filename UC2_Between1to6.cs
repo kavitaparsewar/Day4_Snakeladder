6a4af39be30e2af7378c20addde6c274d38c59d4
kavitaparsewar <parsewarkavita@gmail.com>
2021-09-17 00:09:10 +0530

    UC2 rolls the die between 1 to 6 added

---
+++ UC2_Between1to6.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Day4_SnakeLadder
+{
+    class UC2_Between1to6
+    {
+        public static void randomplayer()
+        {
+            Random random = new Random();
+            Console.WriteLine("Random Die ");
+            for (int die = 0; die < 7; die++)
+            {
+                Console.WriteLine(random.Next(0, 7));
+            }
+
+            //Console.ReadLine();
+        }
+
+    }
+}
